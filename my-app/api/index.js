04eaf8cb2d5bb9847d0e520df96a8c13785130a2
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-04-23 17:18:01 +0500

    Register, Email Verification is completd

--- my-app/api/index.js
+++ my-app/api/index.js
@@ -23,3 +23,94 @@ mongoose.connect('mongodb+srv://E-commerceReact-NativeProject:E-commerceReact-Na
 app.listen(port,()=>{
     console.log("App is running at ",port);
 })
+
+// Register End Point
+const User=require('./models/user');
+const Order=require('./models/order');
+
+const sendVerificationEmail=async (email,verificationToken)=>{
+    // create a Nodemailer Transporter
+
+    const transporter=nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:"shoaibakhter181422@gmail.com",
+            pass:"zqbc kaaf lvjm ryou",
+        },
+    });
+
+    // write email message 
+    const mailOptions={
+        from: "Shoaib.com",
+        to:email,
+        subject:"Email Verification",
+        text:`Please click the following link to verify your email : http://localhost:8000`
+    }
+
+    // send Mail
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Verification email sent successfully");
+    } catch (error) {
+        console.log("Error sending Verification email: ",error);
+    }
+}
+
+app.post('/register',async()=>{
+    try {
+        const {name,email,password} = req.body;
+
+        // check if email is already registered
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            console.log("Email already registered: ", email);
+            return res.status(400).json({message:"Email already registered"})
+        }
+
+        // Create a new user
+        const newUser=await User({name,email,password});
+
+        // Generate and store the verification token
+        newUser.verificationToken = crypto.randomBytes(20).toString("hex");
+        
+        // save the user to the database
+        await newUser.save();
+
+        // Debugging Statement to verify data
+        console.log("New User Created: ",newUser);
+
+        // save verification email to the user
+        // Email Service for sending verification
+
+        sendVerificationEmail(newUser.email, newUser.verificationToken);
+
+        res.status(201).json({
+            message:"Registration Successful: Please check your email for verification"
+        })
+    } catch (error) {
+        console.log("Error during register: ",error);
+        res.status(500).json({message:"Registration Failed"});
+    }
+});
+
+app.get("/verify/:token",async (req,res)=>{
+    try {
+        const token = req.params.token;
+        // Find the user with the given token from the database
+
+        const user=await User.findOne({verificationToken:token});
+        if(!user){
+            return res.status(404).json({message:"Invalid Verification token"});
+        }
+
+        // Mark user as verified
+        user.verified=true;
+        user.verificationToken=undefined;
+
+        await user.save();
+        
+        res.status(200).json({message:"Email Verification Failed"});
+    } catch (error) {
+        res.status(500).json({message:"Email Verification Failed"});
+    }
+})
