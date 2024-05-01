1374acc8de2e8bb24cb0640d4a4878d6aef0b6e0
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-01 11:20:29 +0500

    AddressScreen is completed

--- my-app/navigation/StackNavigator.jsx
+++ my-app/navigation/StackNavigator.jsx
@@ -1,48 +1,60 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import LoginScreen from "../screens/LoginScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import LoginScreen from "../screens/LoginScreen";
 import RegisterScreen from "../screens/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen";
-import ProfileScreen from "../screens/ProfileScreen";
-import CartScreen from "../screens/CartScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Entypo } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { Ionicons } from "@expo/vector-icons";
+import ProductInfoScreen from "../screens/ProductInfoScreen";
+import AddAddressScreen from "../screens/AddAddressScreen";
+import AddressScreen from "../screens/AddressScreen";
+import CartScreen from "../screens/CartScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import ConfirmationScreen from "../screens/ConfirmationScreen";
+import OrderScreen from "../screens/OrderScreen";
 
 const StackNavigator = () => {
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
-
   function BottomTabs() {
     return (
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} 
-        options={{
-          tabBarLabel: 'Home',
-          tabBarLabelStyle:{color:"#008E97"},
-          tabBarIcon:({focused})=>
-          focused?(
-            <Entypo name="home" size={24} color="#008E97" />
-          ):(
-            <AntDesign name="home" size={24} color="black" />
-          )
-        }} />
-        <Tab.Screen name="Profile" component={ProfileScreen} 
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarLabelStyle:{color:"#008E97"},
-          headerShown:false,
-          tabBarIcon:({focused})=>
-          focused?(
-            <Ionicons name="person" size={24} color="#008E97" />
-          ):(
-            <Ionicons name="person-outline" size={24} color="black" />
-          )
-        }} />
-         <Tab.Screen
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: "Home",
+            tabBarLabelStyle: { color: "#008E97" },
+            headerShown: false,
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Entypo name="home" size={24} color="#008E97" />
+              ) : (
+                <AntDesign name="home" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "Profile",
+            tabBarLabelStyle: { color: "#008E97" },
+            tabBarIcon: ({ focused }) =>
+              focused ? (
+                <Ionicons name="person" size={24} color="#008E97" />
+              ) : (
+                <Ionicons name="person-outline" size={24} color="black" />
+              ),
+          }}
+        />
+
+        <Tab.Screen
           name="Cart"
           component={CartScreen}
           options={{
@@ -76,6 +88,32 @@ const StackNavigator = () => {
         <Stack.Screen
           name="Main"
           component={BottomTabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Info"
+          component={ProductInfoScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Address"
+          component={AddAddressScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Add"
+          component={AddressScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Confirm"
+          component={ConfirmationScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Order"
+          component={OrderScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
