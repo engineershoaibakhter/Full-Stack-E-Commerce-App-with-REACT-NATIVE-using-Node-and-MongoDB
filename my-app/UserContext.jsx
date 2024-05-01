bac4462a192ee237f8d8fe1b6968dced4eca83ec
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-01 09:36:31 +0500

    cartReducer is completed

---
+++ my-app/UserContext.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React,{useContext} from 'react'
+
+const UserContext = () => {
+  return (
+    <View>
+      <Text>UserContext</Text>
+    </View>
+  )
+}
+
+export default UserContext
+
+const styles = StyleSheet.create({})
